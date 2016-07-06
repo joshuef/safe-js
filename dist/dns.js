9f73dc67c42fb2949f75f5e0d56f1609572d39b9
Josh Wilson <joshuef@gmail.com>
2016-07-06 13:55:22 +0200

    added babel, updated dist with compiled source

--- dist/dns.js
+++ dist/dns.js
@@ -1,88 +1,91 @@
-import fetch    from 'isomorphic-fetch';
+'use strict';
 
-const SERVER = 'http://localhost:8100/'
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.addService = exports.getServices = exports.getDns = exports.createPublicId = undefined;
 
- export const createPublicId = function( token, longName, callback) {
-    let url = SERVER + 'dns/' + longName;
-    var payload = {
-      method: 'POST',
-      headers: {
-        Authorization: 'Bearer ' + token
-      }
-    };
-    return fetch( url, payload )
-    .then( (response) => {
-        if (response.status !== 200 && response.status !== 206)
-        {
-            console.debug('safe.dns.createPublicId failed with status ' + response.status + ' ' + response.statusText );
-        }
+var _isomorphicFetch = require('isomorphic-fetch');
 
-        return response
-    });
+var _isomorphicFetch2 = _interopRequireDefault(_isomorphicFetch);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var SERVER = 'http://localhost:8100/';
+
+var createPublicId = exports.createPublicId = function createPublicId(token, longName, callback) {
+  var url = SERVER + 'dns/' + longName;
+  var payload = {
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + token
+    }
   };
+  return (0, _isomorphicFetch2.default)(url, payload).then(function (response) {
+    if (response.status !== 200 && response.status !== 206) {
+      console.debug('safe-js.dns.createPublicId failed with status ' + response.status + ' ' + response.statusText);
+    }
 
-  // get dns list
- export const getDns = function( token, callback) {
-    let url = SERVER + 'dns';
-    var payload = {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer ' + token
-      }
-    };
-    return fetch( url, payload )
-    .then( (response) => {
-        if (response.status !== 200 && response.status !== 206)
-        {
-            console.debug('safe.dns.getDns failed with status ' + response.status + ' ' + response.statusText );
-        }
+    return response;
+  });
+};
 
-        return response
-    });
+// get dns list
+var getDns = exports.getDns = function getDns(token, callback) {
+  var url = SERVER + 'dns';
+  var payload = {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + token
+    }
   };
+  return (0, _isomorphicFetch2.default)(url, payload).then(function (response) {
+    if (response.status !== 200 && response.status !== 206) {
+      console.debug('safe-js.dns.getDns failed with status ' + response.status + ' ' + response.statusText);
+    }
 
-  // get service
- export const getServices = function( token, longName, callback) {
-    let url = SERVER + 'dns/' + longName;
-    var payload = {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer ' + token
-      }
-    };
-    return fetch( url, payload )
-    .then( (response) => {
-        if (response.status !== 200 && response.status !== 206)
-        {
-            console.debug('safe.dns.getServices failed with status ' + response.status + ' ' + response.statusText );
-        }
+    return response;
+  });
+};
 
-        return response
-    });
+// get service
+var getServices = exports.getServices = function getServices(token, longName, callback) {
+  var url = SERVER + 'dns/' + longName;
+  var payload = {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + token
+    }
   };
+  return (0, _isomorphicFetch2.default)(url, payload).then(function (response) {
+    if (response.status !== 200 && response.status !== 206) {
+      console.debug('safe-js.dns.getServices failed with status ' + response.status + ' ' + response.statusText);
+    }
 
-  // add service
- export const addService = function( token, longName, serviceName, isPathShared, serviceHomeDirPath, callback) {
-    let url = SERVER + 'dns';
-    var payload = {
-      method: 'PUT',
-      headers: {
-        Authorization: 'Bearer ' + token
-      },
-      data: {
-        longName: longName,
-        serviceName: serviceName,
-        isPathShared: isPathShared,
-        serviceHomeDirPath: serviceHomeDirPath
-      }
-    };
-    return fetch( url, payload )
-    .then( (response) => {
-        if (response.status !== 200 && response.status !== 206)
-        {
-            console.debug('safe.dns.addService failed with status ' + response.status + ' ' + response.statusText );
-        }
+    return response;
+  });
+};
 
-        return response
-    });
+// add service
+var addService = exports.addService = function addService(token, longName, serviceName, isPathShared, serviceHomeDirPath, callback) {
+  var url = SERVER + 'dns';
+  var payload = {
+    method: 'PUT',
+    headers: {
+      Authorization: 'Bearer ' + token
+    },
+    data: {
+      longName: longName,
+      serviceName: serviceName,
+      isPathShared: isPathShared,
+      serviceHomeDirPath: serviceHomeDirPath
+    }
   };
+  return (0, _isomorphicFetch2.default)(url, payload).then(function (response) {
+    if (response.status !== 200 && response.status !== 206) {
+      console.debug('safe-js.dns.addService failed with status ' + response.status + ' ' + response.statusText);
+    }
+
+    return response;
+  });
+};
--- dist/utils.js
+++ dist/utils.js
@@ -1,119 +1,136 @@
-import fetch    from 'isomorphic-fetch';
+'use strict';
 
-const SERVER = 'http://localhost:8100/'
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.authorise = exports.isTokenValid = exports.sendAuthorisationRequest = exports.getUserLongName = exports.setUserLongName = exports.getAuthToken = exports.setAuthToken = undefined;
 
-const TOKEN_KEY         = 'MaidSafeDemoAppTokenReplaceThis';
-const LONG_NAME_KEY     = 'MaidSafeDemoAppLongNameReplaceThis';
-const dnsList           = null;
+var _isomorphicFetch = require('isomorphic-fetch');
 
-export const setAuthToken = function( tokenKey = TOKEN_KEY, token)
-{
-    localStorage.setItem( tokenKey, token );
+var _isomorphicFetch2 = _interopRequireDefault(_isomorphicFetch);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var SERVER = 'http://localhost:8100/';
+
+var TOKEN_KEY = 'MaidSafeDemoAppTokenReplaceThis';
+var LONG_NAME_KEY = 'MaidSafeDemoAppLongNameReplaceThis';
+var dnsList = null;
+
+var setAuthToken = exports.setAuthToken = function setAuthToken() {
+    var tokenKey = arguments.length <= 0 || arguments[0] === undefined ? TOKEN_KEY : arguments[0];
+    var token = arguments[1];
+
+    localStorage.setItem(tokenKey, token);
 };
 
-export const getAuthToken = function( tokenKey = TOKEN_KEY )
-{
-    return localStorage.getItem( tokenKey );
+var getAuthToken = exports.getAuthToken = function getAuthToken() {
+    var tokenKey = arguments.length <= 0 || arguments[0] === undefined ? TOKEN_KEY : arguments[0];
+
+    return localStorage.getItem(tokenKey);
 };
 
-export const setUserLongName = function(longNameKey = LONG_NAME_KEY, longName) {
+var setUserLongName = exports.setUserLongName = function setUserLongName() {
+    var longNameKey = arguments.length <= 0 || arguments[0] === undefined ? LONG_NAME_KEY : arguments[0];
+    var longName = arguments[1];
+
     localStorage.setItem(longNameKey, longName);
 };
 
-export const getUserLongName = function( longNameKey = LONG_NAME_KEY ) {
+var getUserLongName = exports.getUserLongName = function getUserLongName() {
+    var longNameKey = arguments.length <= 0 || arguments[0] === undefined ? LONG_NAME_KEY : arguments[0];
+
     return localStorage.getItem(longNameKey);
 };
 
-export const sendAuthorisationRequest = function( tokenKey, packageData = {} ) {
+var sendAuthorisationRequest = exports.sendAuthorisationRequest = function sendAuthorisationRequest(tokenKey) {
+    var packageData = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
 
-    if( !packageData.productName ||
-        !packageData.identifier ||
-        !packageData.version ||
-        !packageData.author )
-        {
-            throw new Error( 'safe.auth.sendAuthorisationRequest needs package data');
-            return;
+    var url = SERVER + 'auth';
+
+    var authData = {
+        app: {
+            name: packageData.name,
+            id: packageData.id,
+            version: packageData.version,
+            vendor: packageData.vendor
+        },
+        permissions: []
+    };
+
+    var payload = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(authData)
+    };
+
+    return (0, _isomorphicFetch2.default)(url, payload).then(function (response) {
+        var parsedResponse = void 0;
+        if (response.status == 200) {
+            parsedResponse = response.json().then(function (json) {
+                response.__parsedResponseBody__ = json;
+
+                return response;
+            });
         }
 
-        let url = SERVER + 'auth';
-        var payload = {
-            method: 'POST',
-            data: {
-                app: {
-                    name: packageData.productName,
-                    id: packageData.identifier,
-                    version: packageData.version,
-                    vendor: packageData.author
-                },
-                permissions: []
-            }
-        };
-
-        return fetch( url, payload )
-        .then( (response) => {
-            if (response.status !== 200 && response.status !== 206)
-            {
-                console.debug('safe.auth.sendAuthorisationRequest failed with status ' + response.status + ' ' + response.statusText );
-            }
-
-            const body = response.body;
-            const headers = response.headers;
-
-            if (!body && !headers)
-            {
-
-                console.debug('safe.auth.sendAuthorisationRequest failed to connect to Launcher');
-                return('Unable to connect Launcher');
-            }
-
-
-            setAuthToken( tokenKey, body.token);
-
-            return response
-        });;
-    };
-
-    var isTokenValid = function( tokenKey, token ) {
-        var token = token || getAuthToken( tokenKey );
-        if (!token) {
-            return ('No token found');
+        return parsedResponse || response;
+    }).then(function (response) {
+        if (response.status !== 200 && response.status !== 206) {
+            console.debug('safe-js.auth.sendAuthorisationRequest failed with status ' + response.status + ' ' + response.statusText);
         }
 
-        let url = SERVER + 'auth';
-        var payload = {
-            method: 'GET',
-            headers: {
-                Authorization: 'Bearer ' + token
-            }
-        };
+        var body = response.body;
+        var headers = response.headers;
+        var receivedToken = response.__parsedResponseBody__.token;
 
-        return fetch( url, payload )
-        .then( (response) => {
-            if (response.status !== 200 || response.status !== 401  )
-            {
-                console.debug('safe.auth.isTokenValid failed with status ' + response.status + ' ' + response.statusText );
-            }
+        if (!body && !headers) {
 
-            // if( response.status === 200 )
-            // {
-            //     return true;
-            //
-            // }
-            // else {
-            //     return false;
-            // }
-            return response;
-        });;
+            console.debug('safe-js.auth.sendAuthorisationRequest failed to connect to Launcher');
+            return 'Unable to connect Launcher';
+        }
+
+        if (!receivedToken) {
+            console.debug('safe-js.auth.sendAuthorisationRequest failed to parse token from response');
+            return 'Unable to parse token';
+        }
+
+        setAuthToken(tokenKey, receivedToken);
+
+        return response;
+    });
+};
+
+var isTokenValid = exports.isTokenValid = function isTokenValid(tokenKey) {
+    var url = SERVER + 'auth';
+    var token = getAuthToken(tokenKey);
+    var payload = {
+        method: 'GET',
+        headers: {
+            Authorization: 'Bearer ' + token
+        }
     };
 
-    // authorise application
-    export const authorise = function( token )
-    {
-        return isTokenValid( token ).then( response => {
-            if ( !response || response.error ) {
-                localStorage.clear();
-                return sendAuthorisationRequest();
-            }
-            return response;
-        });
-    };
+    return (0, _isomorphicFetch2.default)(url, payload).then(function (response) {
+        if (response.status !== 200 || response.status !== 401) {
+            console.debug('safe-js.auth.isTokenValid failed with status ' + response.status + ' ' + response.statusText);
+        }
+        return response;
+    });
+};
+
+// authorise application
+var authorise = exports.authorise = function authorise(tokenKey, packageData) {
+    console.log("accepted at auth", tokenKey, packageData);
+    return isTokenValid(tokenKey).then(function (response) {
+
+        console.log('in auth', response);
+        if (!response || response.error || response.status === 401) {
+            localStorage.clear();
+            return sendAuthorisationRequest(tokenKey, packageData);
+        }
+        return response;
+    });
+};
