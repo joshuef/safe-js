e5a912ade9bf54dca23f53c91d1451fa61ea5c4c
krishna <krishnaof1988@gmail.com>
2016-09-27 16:57:16 +0530

    export low level apis

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 export * as nfs from './nfs';
 export * as dns from './dns';
 export * as auth from './auth';
+export * as dataId from './data_id';
+export * as appendableData from './appendable_data';
+export * as structuredData from './structured_data';
